45e71095110c4d2b9413301cc1f6725531681d81
BrianBoudrioux <brian1.boudrioux@epitech.eu>
2019-05-22 22:01:23 +0200

    update model schema for user

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -7,45 +7,56 @@ var mongoose = require("mongoose"),
 
 exports.authenticate = function(req, res)
 {
-    if (req.body.password && req.body.email)
+    if (req.body.password)
     {
-        Users.findOne({email: req.body.email}, function(err, user) {
-            if(err) res.json(err);
+        Users.findOne({email: req.body.email}, function(err, user)
+        {
+            if(err) res.status(400).json(err);
 
-            if (user != null) {
+            if (user != null)
+            {
                 var check_pwd = passwordHash.verify(req.body.password, user.password);
+
                 if (check_pwd)
-                    res.json({authenticate: true, message: "sucessfully connected.", user: user});
+                    res.status(200).json({authenticate: true, message: "sucessfully connected.", user: user});
                 else
-                    res.json({authenticate: false, message: "Please verify your email and password."})
+                    res.status(200).json({authenticate: false, message: "Please verify your email and password."})
             }
             else
-                res.json({authenticate: false, message: "Please verify your email and password."})
+                res.status(200).json({authenticate: false, message: "Please verify your email and password."})
         });
     }
     else
-        res.json({authenticate: false, message: "You must define your email and your password for connect to this api."});
+        res.status(200).json({authenticate: false, message: "You must define your email and your password for connect to this api."});
 }
 
 exports.register = function(req, res)
 {
-    if (req.body.password && req.body.email)
+    if (req.body.password && req.body.email && req.body.first_name && req.body.last_name)
     {
-        Users.findOne({email: req.body.email}, function(err, user) {
-            if (err) res.json(err);
-            else if (user == null) {
-                Users.create({ email: req.body.email, password: passwordHash.generate(req.body.password)}, function (err, user) {
-                  if (err) res.json(err);
+        Users.findOne({email: req.body.email}, function(err, user)
+        {
+
+            if (err) res.status(400).json(err);
+
+            else if (user == null)
+            {
+                Users.create({ first_name: req.body.first_name,
+                    last_name: req.body.last_name, email: req.body.email, password: passwordHash.generate(req.body.password)}, function (err, user)
+                {
+
+                  if (err) res.status(400).json(err);
                   else
-                    res.json({created: true, message: "user sucessfully created", user: user})
+                    res.status(201).json({created: true, message: "user sucessfully created", user: user})
+
                 });
             }
             else
-                res.json({authenticate: false, message: "A account allready exist for this email."});
+                res.status(200).json({authenticate: false, message: "A account allready exist for this email."});
 
 
         });
     }
     else
-        res.json({authenticate: false, message: "You must define your email and your password for create your account to this api."});
+        res.status(200).json({authenticate: false, message: "You must define your email and your password for create your account to this api."});
 }
--- api/models/UserModel.js
+++ api/models/UserModel.js
@@ -2,8 +2,35 @@ var mongoose = require("mongoose");
 
 
 var UserSchema = new mongoose.Schema({
-    email : {type: String, lowercase: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true, unique: true},
-    password: {type: String, required: true}
+    first_name: {
+      type: String,
+      required: 'Enter your family name'
+    },
+    last_name: {
+      type: String,
+      required: 'Enter your lastname'
+    },
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      required: "Enter you email please",
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    password: {
+      type: String,
+      required: 'Enter your password'
+    },
+    role: {
+      type: Number,
+      enum: [1, 2, 3],
+      default: 1
+    },
+    created_date: {
+      type: Date,
+      default: Date.now
+    },
 
 });
 
