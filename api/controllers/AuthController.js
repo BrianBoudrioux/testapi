dfdfff25e6e62d82c5fd9988864ba8b34b0ce16c
BrianBoudrioux <brian1.boudrioux@epitech.eu>
2019-05-22 17:19:26 +0200

    create the auth controller

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose"),
+    passwordHash = require("password-hash"),
+    jwt = require("jsonwebtoken"),
+    config = require("../config/config.js");
+    // Users = mongoose.model("Users");
+
+
+exports.authenticate = function(req, res)
+{
+    if (req.body.password && req.body.email)
+    {
+        // Do somethings
+    }
+    else
+        res.json({authenticate: false, message: "You must define your email and your password for connect to this api."});
+}
